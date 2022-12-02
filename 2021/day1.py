42dfd9eec0983642e32af873b0563c3e352fee0d
Filip Brihage <filip.brihage@hotmail.com>
2022-12-02 20:26:34 +0100

    refactor: using zip method to make code more effective

--- 2021/day1.py
+++ 2021/day1.py
@@ -15,17 +15,11 @@
 # Count the number of times a depth measurement increases from the previous measurement.
 # (There is no measurement before the first measurement)
 
-def count_increments(measurements):
-    ctr = 0
-
-    for i in range(len(measurements)):
-        if (i - 1) < 0:
-            continue
-        
-        if measurements[i] > measurements[i - 1]:
-            ctr += 1
-
-    return ctr        
+def count_increments(measurements):      
+    return sum(x < y for x, y in zip(measurements, measurements[1:]))
+    
+    # zip(measurements, measurements[1:]) looks like this:
+    # [(199, 200), (200, 208), (208, 210), (210, 200), (200, 207), (207, 240), (240, 269), (269, 260), (260, 263)]
 
 def main():
     measurements = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
